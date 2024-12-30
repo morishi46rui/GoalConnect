87cd9ab69927c5002cb2e6b8cd0f30c861b83184
宮森 瑠偉 <r.miyamori@eisansystem.jp>
2024-12-31 01:41:50 +0900

    🎉Initial Commit

---
+++ Makefile
@@ -0,0 +1,35 @@
+# 構築
+build: 
+	docker compose build
+buildn:
+	docker compose build --no-cache
+
+# 起動
+up:
+	docker compose up
+upd:
+	docker compose up -d
+
+# 再起動
+re:
+	docker compose restart
+
+# シャットダウン
+down:
+	docker compose down --remove-orphans
+
+# コンテナ内に入る
+b:
+	docker compose exec backend sh
+
+n:
+	docker compose exec nginx sh
+
+d:
+	docker compose exec db sh
+
+# 開発環境構築
+init:
+	cp ./.env.example ./.env && \
+	make buildn && \
+	make upd
---
+++ backend/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	dsn := "host=db user=admin password=secret dbname=goalconnect port=5432 sslmode=disable"
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+	fmt.Println("Connected to the database successfully!")
+
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "OK",
+		})
+	})
+
+	r.Run(":8080")
+}
